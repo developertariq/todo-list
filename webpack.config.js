b6c3ed8af2ee3ca6b7ecd56b31f94073795a5578
developertariq <dev.tariqul.islam@gmail.com>
2022-12-14 12:33:45 +0600

    Hello

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ const isProduction = process.env.NODE_ENV === 'production';
 const stylesHandler = 'style-loader';
 
 const config = {
+  performance: {
+    hints: false,
+  },
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
